d50c787ab3b82983108f8e287db0fb091a869195
ylech <waltermelonson@gmail.com>
2021-02-08 02:06:47 -0700

    Serving statuc

--- node_server/server.js
+++ node_server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 const router = require("./Routes");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.NODE_SERVER_PORT;
 
 if(process.env.NODE_ENV === "production") {
 	app.use(express.static("../build"));
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "../build")));
 
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, "../build/index.html"));
+	res.sendFile(path.resolve("../build/index.html"));
 });
 
 
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
 	console.log(`Server is running on ${PORT}`)
 });
 
