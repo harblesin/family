6faef367e4232f1d51c2475ee7c07ad4dc0d32b9
ylech <waltermelonson@gmail.com>
2021-02-08 22:42:09 -0700

    testing'

--- node_server/server.js
+++ node_server/server.js
@@ -29,7 +29,7 @@ app.use(router);
 console.log(__dirname)
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, "../build/index.html"));
+	res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 
