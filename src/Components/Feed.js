9c8e2f50faddbe7fe545bdcc2d0708502b7cfde1
ylech <waltermelonson@gmail.com>
2021-02-24 23:33:45 -0700

    I'm dumb and reupdating the localhost as ip used in websocket

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -8,7 +8,7 @@ class Feed extends Component {
     }
 
     componentDidMount = () => {
-        const ws = new WebSocket(`ws:192.168.1.3:8080`);
+        const ws = new WebSocket(`ws:localhost:8080`);
         ws.onopen = () => {
             ws.onmessage = (ev) => {
                 let { user, text } = JSON.parse(ev.data);
