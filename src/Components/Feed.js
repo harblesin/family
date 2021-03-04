5c59c9955ed737413b22fe9a43a62f6ca7fb1326
ylech <waltermelonson@gmail.com>
2021-03-03 19:53:26 -0700

    Testing

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -8,7 +8,7 @@ class Feed extends Component {
     }
 
     componentDidMount = () => {
-        const ws = new WebSocket(`${process.env.REACT_APP_ENV === 'production' ? 'wss://192.168.1.3:8080' : 'ws://192.168.1.3:8080'}`);
+        const ws = new WebSocket(`${process.env.REACT_APP_ENV === 'production' ? 'wss://pissbitch.club' : 'ws://localhost:8080'}`);
         // const ws = new WebSocket('ws://192.168.1.3:8080');
         ws.onopen = () => {
             ws.onmessage = (ev) => {
